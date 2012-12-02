1841cd897b0e6edc5a9437ac918b72b30e4f4c12
Jason Irish <jirish@dynamicdoes.com>
2012-12-02 12:20:09 -0600

    initial commit

---
+++ code/SlideImage.php
@@ -0,0 +1,73 @@
+<?php
+
+class SlideImage extends DataObject {
+
+	static $db = array(
+		"Name" => "Text",
+		'SortOrder' => 'Int'
+	);
+	
+	static $has_one = array(
+		"Image" => "Image",
+		"Page" => "Page"
+	);
+	
+	static $singular_name = "Slide";
+	static $plural_name = "Slides";
+	
+	static $default_order = "SortOrder";
+	
+	static $summary_fields = array (
+		'Name' => 'Caption',
+		'GridThumb' => 'Image'
+	);
+	
+	public function GridThumb() {
+		$Image = $this->Image();
+		if ( $Image ) 
+			return $Image->CMSThumbnail();
+		else 
+			return null;
+	}
+	
+	function getCMSFields() {
+	
+		$ImageField = new UploadField('Image', 'Image');
+		$ImageField->getValidator()->allowedExtensions = array('jpg', 'gif', 'png');
+		$ImageField->setFolderName('Uploads/SlideImages');
+		$ImageField->setConfig('allowedMaxFileNumber', 1);
+	   	
+	   	return new FieldList(
+			new TextField('Name'),
+			$ImageField
+		);
+	}
+	
+	function getCMSFields_forPopup() {
+		return new FieldSet(
+			new TextField('Name'),
+			new ImageField('Image')
+			//new DropDown($this->ID,'Product No')
+		);
+	}
+   
+	public function Thumbnail() {
+		return $this->Image()->CroppedImage(80,80);
+	}
+	
+	public function Large() {
+		if ($this->Image()->GetHeight() > 700) {
+			return $this->Image()->SetHeight(700);
+		} else {
+			return $this->Image();
+		}
+	}
+	
+	public function Slide() {
+		return $this->Image()->SetRatioSize(600,500);
+	}
+	
+	function canCreate($member=null) {return true;} 
+	function canEdit($member=null) {return true;} 
+	function canDelete($member=null) {return true;}
+}
