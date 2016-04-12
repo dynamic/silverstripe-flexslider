862d4a5016b6e98cb415a5a6a4a6b6afeae202dc
Jason Irish <jirish@dynamicagency.com>
2016-04-11 20:34:58 -0500

    quick psr-2 cleanup

--- tests/FlexSliderTest.php
+++ tests/FlexSliderTest.php
@@ -1,24 +1,27 @@
 <?php
 
-class FlexSliderTest extends FunctionalTest{
+class FlexSliderTest extends FunctionalTest
+{
+    protected static $fixture_file = 'flexslider/tests/FlexSliderTest.yml';
+    protected static $disable_themes = true;
+    protected static $use_draft_site = false;
 
-	protected static $fixture_file = 'flexslider/tests/FlexSliderTest.yml';
-	protected static $disable_themes = true;
-	protected static $use_draft_site = false;
+    public function setUp()
+    {
+        parent::setUp();
 
-	public function setUp(){
-		parent::setUp();
+        ini_set('display_errors', 1);
+        ini_set('log_errors', 1);
+        error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+    }
 
-		ini_set('display_errors', 1);
-		ini_set("log_errors", 1);
-		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
-	}
+    public function logOut()
+    {
+        $this->session()->clear('loggedInAs');
+        $this->session()->clear('logInWithPermission');
+    }
 
-	public function logOut(){
-		$this->session()->clear('loggedInAs');
-		$this->session()->clear('logInWithPermission');
-	}
-
-	public function testFlexSlider(){}
-
+    public function testFlexSlider()
+    {
+    }
 }
