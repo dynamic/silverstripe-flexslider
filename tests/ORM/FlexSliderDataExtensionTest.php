7a0bbf3882ba2c52e59028b53be2299834bad94b
Nic Horstmeier <nhorstmeier@gmail.com>
2017-04-06 12:10:39 -0500

    UPDATE tests

--- tests/ORM/FlexSliderDataExtensionTest.php
+++ tests/ORM/FlexSliderDataExtensionTest.php
@@ -7,6 +7,7 @@ use Dynamic\FlexSlider\ORM\FlexSlider;
 use SilverStripe\Core\Config\Config;
 use Dynamic\FlexSlider\Test\TestOnly\TestOnlyPage;
 use SilverStripe\ORM\DataList;
+use SilverStripe\Core\Injector\Injector;
 
 /**
  * Class FlexSliderDataExtensionTest
@@ -15,17 +16,19 @@ use SilverStripe\ORM\DataList;
 class FlexSliderDataExtensionTest extends FlexSliderTest
 {
 
+    /**
+     * @var array
+     */
     protected static $extra_dataobjects = array(
         TestOnlyPage::class,
     );
 
     public function testTabNameConfig()
     {
-        $page = new TestOnlyPage();
+        $page = TestOnlyPage::create();
         $page->write();
-        $extension = new FlexSlider();
         $pageFields = $page->getCMSFields();
-        $extension->updateCMSFields($pageFields);
+        //$extension->updateCMSFields($pageFields);
         $this->assertNotNull($pageFields->fieldByName('Slides'));
 
         Config::modify()
@@ -33,28 +36,22 @@ class FlexSliderDataExtensionTest extends FlexSliderTest
         $page2 = TestOnlyPage::create();
         $page2->write();
         $page2Fields = $page2->getCMSFields();
-        $extension->updateCMSFields($page2Fields);
+        //$extension->updateCMSFields($page2Fields);
         $this->assertNull($page2Fields->fieldByName('Root.Slides'));
         $this->assertNotNull($page2Fields->fieldByName('Root.MyCustomSlideTitle'));
     }
 
     public function testUpdateCMSFields()
     {
-        $extension = new FlexSlider();
         $object = TestOnlyPage::create();
-        $fields = $object->getCMSFields();
-        $extension->updateCMSFields($fields);
-        $this->assertNull($fields->dataFieldByName('Slides'));
-
         $object->write();
         $fields = $object->getCMSFields();
-        $extension->updateCMSFields($fields);
         $this->assertNotNull($fields->dataFieldbyName('Slides'));
     }
 
     public function testGetSlideShow()
     {
-        $object = singleton(TestOnlyPage::class);
+        $object = TestOnlyPage::create();
         $object->write();
         $slide1 = $this->objFromFixture(
             'Dynamic\\FlexSlider\\Model\\SlideImage',
@@ -63,7 +60,7 @@ class FlexSliderDataExtensionTest extends FlexSliderTest
         /*$image = $this->objFromFixture('Image', 'image1');
         $slide1->ImageID = $image->ID;*/
         $object->Slides()->add($slide1);
-        $slides = $object->SlideShow();
+        $slides = $object->getSlideShow();
         $this->assertInstanceOf(DataList::class, $slides);
     }
 }
