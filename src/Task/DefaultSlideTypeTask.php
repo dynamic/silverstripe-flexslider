acdb09fbe009c1fb81b1a9509d49e9a11f079e26
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2019-02-05 15:04:47 -0600

    ENHANCEMENT SlideType default value task
    BUGFIX check if LinkLabel isset in SlideLinkTask

---
+++ src/Task/DefaultSlideTypeTask.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Dynamic\Flexslider\Task;
+
+use Dynamic\FlexSlider\Model\SlideImage;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\ORM\DB;
+
+class DefaultSlideTypeTask extends BuildTask
+{
+    /**
+     * @var string
+     */
+    private static $segment = 'default-slide-type-task';
+
+    /**
+     * @var string
+     */
+    protected $title = 'Flexslider - Default Slide Type Task';
+
+    /**
+     * @param \SilverStripe\Control\HTTPRequest $request
+     */
+    public function run($request)
+    {
+        $this->setDefaults();
+    }
+
+    /**
+     *
+     */
+    protected function setDefaults()
+    {
+        $default = SlideImage::singleton()->config()->get('defaults');
+
+        if (isset($default['SlideType'])) {
+            $baseTable = SlideImage::singleton()->baseTable();
+
+            $tables = [
+                $baseTable,
+                "{$baseTable}_Versions",
+                "{$baseTable}_Live",
+            ];
+
+            foreach ($tables as $table) {
+                foreach ($this->yieldSingle(DB::query("SELECT * FROM \"{$table}\" WHERE \"SlideType\" IS NULL")) as $record) {
+                    DB::prepared_query(
+                        "UPDATE \"{$table}\" SET \"SlideType\" = ? WHERE \"ID\" = ?",
+                        [$default['SlideType'], $record['ID']]
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $list
+     * @return \Generator
+     */
+    protected function yieldSingle($list)
+    {
+        foreach ($list as $item) {
+            yield $item;
+        }
+    }
+}
