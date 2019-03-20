08b7c63f605bde5216577763af1a6761504f0488
mak001 <mak001001@gmail.com>
2019-03-20 11:53:07 -0500

    Added the ability to set slider speed in the config

--- src/ORM/FlexSlider.php
+++ src/ORM/FlexSlider.php
@@ -200,9 +200,7 @@ class FlexSlider extends DataExtension
             ? $this->owner->flexSliderAfterAction()
             : 'function(){}';
 
-        $speed = $this->owner->hasMethod('setFlexSliderSpeed')
-            ? $this->owner->setFlexSliderSpeed()
-            : 7000;
+        $speed = $this->getSlideshowSpeed();
 
         Requirements::customScript(
             "(function($) {
@@ -252,6 +250,24 @@ class FlexSlider extends DataExtension
                 });
             }(jQuery));'
         );
+    }
+
+    /**
+     * @return int
+     */
+    private function getSlideshowSpeed()
+    {
+        if ($this->owner->hasMethod('setFlexSliderSpeed') && $this->owner->setFlexSliderSpeed()) {
+            return $this->owner->setFlexSliderSpeed();
+        }
+
+        // check the config for an integer
+        $configSpeed = $this->owner->config()->get('FlexSliderSpeed');
+        if ($configSpeed && (int) $configSpeed == $configSpeed) {
+            return $this->owner->config()->get('FlexSliderSpeed');
+        }
+
+        return 7000;
     }
 
     /**
